b3f5cdd926267f7bc26bbdcdac104999cf078d51
TheCookieLeg <lucas2.olafsen@gmail.com>
2025-04-08 11:17:02 +0200

    Movement and rotation is way smoother

--- P2UnityProduct/Assets/LucasDragging/Scripts/Dragging.cs
+++ P2UnityProduct/Assets/LucasDragging/Scripts/Dragging.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEditor;
 using UnityEngine;
 
 public class Dragging : MonoBehaviour
 {
-    private Vector3 offset; // offset between the touch point and the center of the object
+    [Header("Dragging variables")]
+    private UnityEngine.Vector3 offset; // offset between the touch point and the center of the object
     [SerializeField] private bool isDragging = false;
     private float zDepth;
+
+    [Header("Rotation Variables")]
+    private UnityEngine.Vector2 oldPos;
+    private UnityEngine.Vector2 newPos;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +25,7 @@ public class Dragging : MonoBehaviour
     void Update()
     {
         DragAndDrop();
+        RotateObject();
     }
 
     private void DragAndDrop()
@@ -28,28 +35,49 @@ public class Dragging : MonoBehaviour
             Debug.Log("I GOT TOUCHED");
             Touch touch = Input.GetTouch(0); // Gets info on the first finger that touches the screen (struct variable)
 
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, zDepth));
+            UnityEngine.Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new UnityEngine.Vector3(touch.position.x, touch.position.y, zDepth));
 
             // TouchPhase.Began is the first frame that the finger touches the screen. Here we can calculate some values
             if (touch.phase == TouchPhase.Began)
             {
-                Vector2 touchWorldPos2D = Camera.main.ScreenToWorldPoint(touch.position);
-                RaycastHit2D hit = Physics2D.Raycast(touchWorldPos2D, Vector2.zero);
+                UnityEngine.Vector2 touchWorldPos2D = Camera.main.ScreenToWorldPoint(touch.position);
+                RaycastHit2D hit = Physics2D.Raycast(touchWorldPos2D, UnityEngine.Vector2.zero);
 
                 if (hit.collider != null && hit.transform == transform)
                 {
                     isDragging = true;
-                    offset = transform.position - (Vector3)touchWorldPos2D;
+                    offset = transform.position - (UnityEngine.Vector3)touchWorldPos2D;
                 }
             }
             else if (touch.phase == TouchPhase.Moved && isDragging)
             {
-                transform.position = new Vector3(touchPosition.x + offset.x, touchPosition.y + offset.y, touchPosition.z + offset.z);
+                UnityEngine.Vector3 targetPos = new UnityEngine.Vector3(touchPosition.x + offset.x, touchPosition.y + offset.y, touchPosition.z + offset.z);
+                transform.position = UnityEngine.Vector3.Lerp(transform.position, targetPos, 0.4f);
             }
             else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
             {
                 isDragging = false;
             }
+        }
+    }
+
+    private void RotateObject()
+    {
+        if (isDragging)
+        {
+            newPos = transform.position;
+            UnityEngine.Vector2 movement = newPos - oldPos;
+
+            if (movement.magnitude > 0.01f)
+            {
+                UnityEngine.Quaternion targetRotation = UnityEngine.Quaternion.LookRotation(UnityEngine.Vector3.forward, movement.normalized);
+
+                transform.rotation = UnityEngine.Quaternion.Lerp(transform.rotation, targetRotation, 0.4f);               
+                
+                oldPos = newPos;
+            }
+
+            
         }
     }
 }
