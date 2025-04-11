6731239350aac2484f4c6cbe6b1a6f491fb9fbd9
TheCookieLegend <lucas2.olafsen@gmail.com>
2025-04-11 15:07:29 +0200

    Revert "Merge branch 'LucasGame'"
    This reverts commit 0abc064a40e70c9200c0a428f093b9913504ca41, reversing
    changes made to fcf6aaf09f35a4d341e47422868046ba2f88683f.

--- P2UnityProduct/Assets/LucasDragging/Scripts/FabricSpawner.cs
+++ P2UnityProduct/Assets/LucasDragging/Scripts/FabricSpawner.cs
@@ -1,71 +1,25 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FabricSpawner : MonoBehaviour
 {
     public GameObject fabric;
-    public GameObject Scorer;
     [SerializeField, Tooltip("The distance between each piece of fabric")] private float distance = 1f;
     [SerializeField, Tooltip("Number of fabrics to spawn")] private int repetitions;
+    private Vector3 spawnPoint;
 
-    private List<GameObject> fabricPositions = new List<GameObject>();
-    private int nextScorerIndex = 0;
-    private int index = 0;
-    
-    private int scorerBaseIndex = 0;
-    private bool moveForward = true;
     void Start()
     {
-        Vector3 fabricSpawnPoint = transform.position;
-
-        // Spawn fabrics and store their positions
+        spawnPoint = transform.position;
         for (int i = 0; i < repetitions; i++)
         {
-            fabricPositions.Add(Instantiate(fabric, fabricSpawnPoint, quaternion.identity, gameObject.transform));
-            fabricSpawnPoint += new Vector3(distance, 0, 0);
+            Instantiate(fabric, spawnPoint, quaternion.identity);
+            spawnPoint += new Vector3(distance, 0, 0);
         }
-        SpawnScorerFirstTime();
     }
 
-    public void SpawnScorer()
-    {
-        if (scorerBaseIndex < 0 || scorerBaseIndex + 1 >= fabricPositions.Count)
-        {
-            Debug.LogWarning("Out of bounds - can't spawn scorer.");
-            return;
-        }
-
-        Vector3 posA = fabricPositions[scorerBaseIndex].transform.position;
-        Vector3 posB = fabricPositions[scorerBaseIndex + 1].transform.position;
-        Vector3 midpoint = (posA + posB) / 2;
-
-        Instantiate(Scorer, midpoint, quaternion.identity, gameObject.transform);
-        
-        if (moveForward)
-        {
-            scorerBaseIndex += 2;
-        }
-        else
-        {
-            scorerBaseIndex -= 1;
-        }
-
-        moveForward = !moveForward; // Flip direction every time
-    }
-
-    private void SpawnScorerFirstTime()
-    {
-        Vector3 posA = fabricPositions[nextScorerIndex].transform.position;
-        Vector3 posB = fabricPositions[nextScorerIndex + 1].transform.position;
-        Vector3 midpoint = (posA + posB) / 2;
-
-        Instantiate(Scorer, midpoint, quaternion.identity, gameObject.transform);
-        //nextScorerIndex++;
-    }
-
-    private bool isEven(int value)
-    {
-        return value % 2 == 0;
-    }
 }
