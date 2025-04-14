527d909b1cac217672c505a27d62c7bd1ea78d10
TheCookieLeg <lucas2.olafsen@gmail.com>
2025-04-14 10:18:19 +0200

    Fabric moves in FixedUpdate now

--- P2UnityProduct/Assets/LucasDragging/Scripts/FabricMovement.cs
+++ P2UnityProduct/Assets/LucasDragging/Scripts/FabricMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class FabricMovement : MonoBehaviour
 {
-    private bool moveTriggered = false;
     public float moveSpeed = 0.01f;
 
-    public void StartMovement()
+    void Start()
+    {
+        enabled = false;
+    }
+
+    void FixedUpdate()
     {
         transform.position = new Vector3(transform.position.x - moveSpeed, transform.position.y, transform.position.z);
     }
